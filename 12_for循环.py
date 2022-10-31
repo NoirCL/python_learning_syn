b66fc9b0122e3002684d572f818d9e009c8067ed
NoirCL <chenling774516@gmail.com>
2022-10-31 21:03:24 +0800

    Default Changelist

--- 12_for循环.py
+++ 12_for循环.py
@@ -1,3 +1,16 @@
 # Chlorine 
 # Time : 2022/10/31 20:58
 
+"""
+    1. for 和 while的区别：
+        1. for循环是一种轮询的机制，是对一批内容就进行逐个处理的模式
+        2. while循环的条件是自定义的，自行控制循环条件
+    2. for语句
+        for 临时变量 in 待处理的数据集：
+            statement
+"""
+
+###
+for char in "Hello World":
+    print(char)
+###
